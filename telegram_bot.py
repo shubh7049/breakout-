7190fdd36838606a385b73037ac7076fb214f440
shubh7049 <shakya.shubham11@gmail.com>
2025-06-20 11:31:57 +0530

    Update telegram_bot.py

--- telegram_bot.py
+++ telegram_bot.py
@@ -1,1 +1,4 @@
 # Telegram bot integration using python-telegram-bot
+import streamlit as st
+bot_token = st.secrets["telegram"]["7771962486:AAEOxI1T80ruh2ay0VL5iuE1y00idwvWcPQ"]
+chat_id = st.secrets["telegram"]["shubhjux"]
